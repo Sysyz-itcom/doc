ec0a8ae6d7f1363e4bbcd248d72657d19e5ec010
ITCOM <159421917+Sysyz-itcom@users.noreply.github.com>
2025-01-25 18:26:34 +0800

    Create lazy-img.js

---
+++ components/js/lazy-img.js
@@ -0,0 +1,16 @@
+// 图片懒加载
+// 获取所有懒加载图片
+const lazyImages = document.querySelectorAll(".lazy");
+
+// 监听页面滚动
+const loadLazyImages = () => {
+    lazyImages.forEach(image => {
+        if (image.getBoundingClientRect().top < window.innerHeight && !image.src) {
+            image.src = image.getAttribute("data-src");
+        }
+    });
+};
+
+// 初始化懒加载
+window.addEventListener("scroll", loadLazyImages);
+loadLazyImages(); // 页面加载时检查一次
